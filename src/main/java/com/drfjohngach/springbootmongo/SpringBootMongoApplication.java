bce84b1e034345d06ba28d1edbe96ab073e10060
drfjohngach <johngachungagitau@gmail.com>
2024-03-20 16:26:29 +0300

    Inserting new student to the database using @Bean annotation

--- src/main/java/com/drfjohngach/springbootmongo/SpringBootMongoApplication.java
+++ src/main/java/com/drfjohngach/springbootmongo/SpringBootMongoApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootMongoApplication {
 
@@ -12,5 +16,21 @@ public class SpringBootMongoApplication {
 		SpringApplication.run(SpringBootMongoApplication.class, args);
 	}
 
-
+	@Bean
+	CommandLineRunner runner(StudentRepository repository){
+		return args -> {
+			Address address = new Address("England","London","NW9");
+			Student student = new Student(
+					"Jamilla",
+					"Ahmed",
+					"jamilla@gmail.com",
+					Gender.FEMALE,
+					address,
+					List.of("Computer Science"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+			repository.insert(student);
+		};
+	}
 }
--- src/main/java/com/drfjohngach/springbootmongo/Student.java
+++ src/main/java/com/drfjohngach/springbootmongo/Student.java
@@ -23,4 +23,16 @@ public class Student {
     private List<String> favouriteSubjects;
     private BigDecimal totalSpentOnBooks;
     private LocalDateTime createdAt;
+
+    public Student(String firstName,
+                   String lastName, String email, Gender gender, Address address, List<String> favouriteSubjects, BigDecimal totalSpentOnBooks, LocalDateTime createdAt) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.address = address;
+        this.favouriteSubjects = favouriteSubjects;
+        this.totalSpentOnBooks = totalSpentOnBooks;
+        this.createdAt = createdAt;
+    }
 }
