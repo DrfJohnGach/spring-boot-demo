6c5704cc69655ce598cae6032a7bd8821598cbab
drfjohngach <johngachungagitau@gmail.com>
2024-03-20 16:05:28 +0300

    Model student

---
+++ src/main/java/com/drfjohngach/springbootmongo/Gender.java
@@ -0,0 +1,5 @@
+package com.drfjohngach.springbootmongo;
+
+public enum Gender {
+    MALE, FEMALE
+}
--- src/main/java/com/drfjohngach/springbootmongo/SpringBootMongoApplication.java
+++ src/main/java/com/drfjohngach/springbootmongo/SpringBootMongoApplication.java
@@ -1,7 +1,9 @@
 package com.drfjohngach.springbootmongo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootMongoApplication {
@@ -9,5 +11,6 @@ public class SpringBootMongoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMongoApplication.class, args);
 	}
+
 
 }
